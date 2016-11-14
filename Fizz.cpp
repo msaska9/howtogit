ccb5a6342119807e9f45c0ef8f5c3fa67f4ea139
msaska9 <molnar.saska.zoltan@gmail.com>
2016-11-14 14:03:28 +0100

    buzz function added to fizz

--- Fizz.cpp
+++ Fizz.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+void Buzz()
+{
+    for(int i=1; i<=100; i++)
+    {
+        if(i%4==0) cout<<"Fizz"<<endl;
+        else cout<<i<<endl;
+    }
+}
 int main()
 {
     for(int i=1; i<=100; i++)
