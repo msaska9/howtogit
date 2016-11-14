e7276ad49fa9e0248d9c0090fd70959218f28631
msaska9 <molnar.saska.zoltan@gmail.com>
2016-11-14 13:37:21 +0100

    mod3fizz

--- Fizz.cpp
+++ Fizz.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main()
 {
-    for(int i=1; i<=100; i++) cout<<i<<endl;
+    for(int i=1; i<=100; i++)
+    {
+        if(i%3==0) cout<<"Fizz"<<endl;
+        else cout<<i<<endl;
+    }
 }
